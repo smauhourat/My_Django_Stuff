b9d9bab5f8ac9d5aa58778e8a387d69f0361ce2d
smauhourat <santiagomauhourat@hotmail.com>
2020-12-14 15:12:58 -0300

    boot post validator

--- basicforms/basicapp/forms.py
+++ basicforms/basicapp/forms.py
@@ -5,3 +5,10 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    bootcatcher = forms.CharField(required=False, widget=forms.HiddenInput)
+
+    def clean_bootcatcher(self):
+        bootcatcher = self.cleaned_data['bootcatcher']
+        if len(bootcatcher) > 0:
+            raise forms.ValidationError('GOTCHA BOT!')
+        return bootcatcher
