edde6bb12d9885e1753c566ee3c766f970301395
smauhourat <santiagomauhourat@hotmail.com>
2020-12-29 09:27:23 -0300

    registration form

---
+++ django_level_five/learning_users/basic_app/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import path, re_path, include
+from basic_app import views
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    re_path(r'^register/$', views.register, name='register'),
+    # re_path(r'^other/$', views.other, name='other')
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
