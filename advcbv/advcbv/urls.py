c91549f5e5b6e92cbf9333ab94c810b23d7bf683
smauhourat <santiagomauhourat@hotmail.com>
2020-12-30 11:41:09 -0300

    add index view

--- advcbv/advcbv/urls.py
+++ advcbv/advcbv/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from basic_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # re_path(r'^$',  views.index, name="index"),
+    # re_path(r'^$',  views.CBView.as_view()),
+    # path('', views.CBView.as_view())
+    path('', views.IndexView.as_view())
 ]
--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
+from django.http import HttpResponse
 
-# Create your views here.
+
+# def index(request):
+#     return render(request, 'basic_app/index.html')
+
+# class CBView(View):
+#     def get(self, request):
+#         return HttpResponse("CLASS BASED VIEWS ARE COOL!!")
+
+class IndexView(TemplateView):
+    template_name = 'basic_app/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION!'
+        return context
