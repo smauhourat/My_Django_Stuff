6562c0cfef2dda6483c170494618b487fc8442ef
smauhourat <santiagomauhourat@hotmail.com>
2020-12-31 09:13:08 -0300

    update and delete school

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 from django.http import HttpResponse
 from . import models
 
@@ -22,8 +24,8 @@ from . import models
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self,**kwargs):
-        context  = super().get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context['injectme'] = "Basic Injection!"
         return context
 
@@ -37,3 +39,18 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
